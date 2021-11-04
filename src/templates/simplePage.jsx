60c61f8b90b7fd1d19b855836ce9a8feab6340cf
vincentreynaud <vincentreynaud.de@gmail.com>
2021-11-04 15:48:57 +0100

    adjust simplePage  styles

--- src/templates/simplePage.jsx
+++ src/templates/simplePage.jsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import Back from "../components/Back";
 import Layout from "../components/Layout";
 
+import "../scss/main.scss";
+
 export default function SimplePage({ pageContext }) {
   const { id, html } = pageContext;
 
